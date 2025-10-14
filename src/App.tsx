acac3549755f330fafecf78ccfa1e0ecedda8212
d3haka <arian.rassaee@gmail.com>
2025-10-14 20:47:59 +0330

    feat: implemented category pills & content grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
+import { useState } from "react";
 import { CategoryPills } from "./components/category-pills";
-import { categories } from "./data/home";
+import { categories, videos } from "./data/home";
 import { PageHeader } from "./layouts/page-header";
 import "./styles.css";
+import { VideoGridItem } from "./components/video-grid-item";
+import { Sidebar } from "./layouts/sidebar";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+
   return (
     <div className="max-h-screen flex flex-col ">
       <PageHeader />
-      <div className="grid grid-cols-[auto,1fr] grow-[1] overflow-auto">
-        <div>sidebar</div>
-        <div className="sticky top-0 bg-white z-10 pb-4">
-          <CategoryPills categories={categories} />
+      <div className="grid grid-cols-[auto_1fr] grow overflow-auto">
+        <Sidebar />
+        <div className="overflow-x-hidden px-8 pb-4">
+          <div className="sticky top-0 bg-white z-10 pb-4">
+            <CategoryPills
+              categories={categories}
+              selectedCategory={selectedCategory}
+              onSelect={setSelectedCategory}
+            />
+          </div>
+          <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+            {videos.map((video) => (
+              <VideoGridItem key={video.id} {...video} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
